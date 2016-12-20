b65a3945cbb4b44d81d8b21274d582d7c862d361
Lucas Cherkewski <lucas@ecustom.ca>
2016-12-20 17:22:53 -0500

    hover style for button

--- src/components/Buttons/BasicButton.js
+++ src/components/Buttons/BasicButton.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 class BasicButton extends React.Component {
   render() {
     let buttonClasses = classNames({
-      'input-reset f6 pa2 sans-serif pointer bg-light-gray ba b--black': true,
+      'input-reset f6 pa2 sans-serif pointer bg-light-gray hover-bg-moon-gray ba b--black': true,
       [this.props.className]: true
     });
 

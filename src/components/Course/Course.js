4a573aaaa61611e282a57148f76513bcea1d1e56
Lucas Cherkewski <lucas@ecustom.ca>
2016-12-19 21:06:54 -0500

    separate CourseInfo into its own component

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,29 +2,9 @@ import React from 'react';
 
 import classNames from 'classnames';
 
-import CourseInterestButton from './CourseInterestButton';
+import CourseInfo from './CourseInfo';
 
 class Course extends React.Component {
-  constructor() {
-    super();
-
-    this.renderCourseInfo = this.renderCourseInfo.bind(this);
-  }
-
-  renderCourseInfo() {
-    return (
-      <div className="pb4">
-        <p className="lh-copy serif mb0">{this.props.course.description}</p>
-
-        <ul className="list pl0 mt2">
-          <li className="gray dib f6">{this.props.course.code}</li>
-        </ul>
-
-        {CourseInterestButton(this.props.isAdded)}
-      </div>
-    )
-  }
-
   render() {
     let headerClasses = classNames({
       'bb bw1 pb1 lh-title mt0 hover-black-80 pointer link': true,
@@ -35,7 +15,7 @@ class Course extends React.Component {
       <li className="b--black-30 pv1 mt2 measure" key={this.props.course.code}>
         <h3 className={headerClasses} onClick={this.props.toggleOpenState}>{this.props.course.title}</h3>
 
-        {this.props.isOpen ? this.renderCourseInfo() : null}
+        {this.props.isOpen ? <CourseInfo {...this.props}/> : null}
       </li>
     )
   }
---
+++ src/components/Course/CourseInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import CourseInterestButton from './CourseInterestButton';
+
+const CourseInfo = (props) => {
+  return (
+    <div className="pb4">
+      <p className="lh-copy serif mb0">{props.course.description}</p>
+
+      <ul className="list pl0 mt2">
+        <li className="gray dib f6">{props.course.code}</li>
+      </ul>
+
+      {CourseInterestButton(props.isAdded)}
+    </div>
+  )
+};
+
+export default CourseInfo;

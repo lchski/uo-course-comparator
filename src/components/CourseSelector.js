21b1f24181daf0ab8762ab5bc2077f22eae7d249
Lucas Cherkewski <lucas@ecustom.ca>
2016-12-21 09:52:41 -0500

    set up rendering for no department selected

--- src/components/CourseSelector.js
+++ src/components/CourseSelector.js
@@ -6,8 +6,11 @@ class CourseSelector extends React.Component {
   constructor() {
     super();
 
+    this.switchDepartment = this.switchDepartment.bind(this);
+    this.renderDepartmentSection = this.renderDepartmentSection.bind(this);
+
     this.state = {
-      currentDepartment: '',
+      currentDepartment: null,
       courses: []
     };
   }
@@ -16,13 +19,30 @@ class CourseSelector extends React.Component {
     this.setState({ currentDepartment: department });
   }
 
+  renderDepartmentSection(currentDepartment) {
+    return (
+      <div className="pv3 measure">
+        <h2 className="lh-title">{currentDepartment.name}</h2>
+
+        <CourseList
+          toggleInterestedCourse={this.props.toggleInterestedCourse}
+          interestedCourses={this.props.interestedCourses}
+          courses={this.props.courses}
+        />
+      </div>
+    )
+  }
+
   render() {
+    let currentDepartment = this.props.departments.find((department) => department.code === this.state.currentDepartment);
+
     return (
       <div>
         <p className="measure-narrow lh-copy">Welcome to the course comparator. To begin, select a department whose courses you’d like to view.</p>
 
         <label htmlFor="department" className="db i">Department</label>
         <select id="department" className="w-100 mt2" onChange={(e) => this.switchDepartment(e.target.value)}>
+          <option value={null}>---No department---</option>
           {
             this.props.departments.map((department) => {
               return <option key={department.code} value={department.code}>{department.name}</option>
@@ -30,15 +50,7 @@ class CourseSelector extends React.Component {
           }
         </select>
 
-        <div className="pv3 measure">
-          <h2 className="lh-title">Political Science</h2>
-
-          <CourseList
-            toggleInterestedCourse={this.props.toggleInterestedCourse}
-            interestedCourses={this.props.interestedCourses}
-            courses={this.props.courses}
-          />
-        </div>
+        {(typeof currentDepartment != 'undefined') ? this.renderDepartmentSection(currentDepartment) : null}
       </div>
     )
   }

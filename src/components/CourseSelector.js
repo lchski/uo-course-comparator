1ae6c66ce122e3efb1c30f02c5e75337d099d846
Lucas Cherkewski <lucas@ecustom.ca>
2016-12-21 09:45:50 -0500

    set up department switching

--- src/components/CourseSelector.js
+++ src/components/CourseSelector.js
@@ -3,13 +3,26 @@ import React from 'react';
 import CourseList from './CourseList';
 
 class CourseSelector extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentDepartment: '',
+      courses: []
+    };
+  }
+
+  switchDepartment(department) {
+    this.setState({ currentDepartment: department });
+  }
+
   render() {
     return (
       <div>
         <p className="measure-narrow lh-copy">Welcome to the course comparator. To begin, select a department whose courses you’d like to view.</p>
 
         <label htmlFor="department" className="db i">Department</label>
-        <select id="department" className="w-100 mt2">
+        <select id="department" className="w-100 mt2" onChange={(e) => this.switchDepartment(e.target.value)}>
           {
             this.props.departments.map((department) => {
               return <option key={department.code} value={department.code}>{department.name}</option>

12270003ebc9d67aa52fd010a1e6410f4fde8b4c
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-03 11:38:51 -0400

    output extra course details

--- src/components/Course/CourseInfo.js
+++ src/components/Course/CourseInfo.js
@@ -7,12 +7,14 @@ const CourseInfo = (props) => {
     <div className="pb4">
       <p className="lh-copy serif mb0">{props.course.description}</p>
 
-      <p className="lh-copy sans-serif gray f6 pt2 mt2 bt bw1 b--light-gray">{props.course.restriction}</p>
+      <div className="pt2 mt3 bt bw1 b--light-gray">
+        {props.course.extraDetails.map((detail, index) => <p key={props.course.code + index} className="mt2 mb0 lh-copy sans-serif gray f6">{detail}</p>)}
 
-      <ul className="list pl0 mt2">
-        <li className="gray dib f6 mr3">{props.course.code}</li>
-        <li className="gray dib f6">Year {props.course.year}</li>
-      </ul>
+        <ul className="list pl0 mt2">
+          <li className="gray dib f6 mr3">{props.course.code}</li>
+          <li className="gray dib f6">Year {props.course.year}</li>
+        </ul>
+      </div>
 
       <CourseInterestButton isAdded={props.isAdded} onClick={props.toggleInterestedCourse}/>
     </div>
